4fd01e39547b770893c9e9ddb508ea747f0f327c
Suhyun Jeong <thebbbb1206@gmail.com>
2023-07-27 07:29:42 +0900

    사진 추가, 각 행 4명 배치

--- src/pages/developers/GraduateList.js
+++ src/pages/developers/GraduateList.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './GraduateList.css';
-import img from './image/img.PNG';
+import images from './image/index.js';
 import Header from "../../Header/header_new"
 
 const GraduateList = () => {
     const members = [
-        { name: "강수연", groupName: ""},
+        { name: "강수연", groupName: "HOT GIRL"},
         { name: "구연우" , groupName: "UNICORN" },
         { name: "김선경" , groupName: "SELECT" },
         { name: "김유정" , groupName: "UNICORN" },
-        { name: "김지혜" , groupName: "" },
-        { name: "노정우" , groupName: "" },
-        { name: "문지영" , groupName: "ADMIN" },
-        { name: "박상은" , groupName: "" },
-        { name: "박세림" , groupName: "" },
-        { name: "박소정" , groupName: "" },
+        { name: "김지혜" , groupName: "FULL HOUSE" },
+        { name: "노정우" , groupName: "FULL HOUSE" },
+        { name: "문지영" , groupName: "덕성전자" },
+        { name: "박상은" , groupName: "개발JEANS" },
+        { name: "박세림" , groupName: "개발JEANS" },
+        { name: "박소정" , groupName: "빵야" },
         { name: "박은비" , groupName: "UNICORN" },
-        { name: "박해인" , groupName: "ADMIN" },
+        { name: "박해인" , groupName: "덕성전자" },
         { name: "양예지" , groupName: "SELECT" },
         { name: "유다영" , groupName: "UNICORN" },
-        { name: "이나래" , groupName: "" },
-        { name: "이소영(1)" , groupName: "" },
-        { name: "이소영(2)" , groupName: "" },
-        { name: "이수진" , groupName: "ADMIN" },
-        { name: "이유진" , groupName: "" },
+        { name: "이나래" , groupName: "개발JEANS" },
+        { name: "이소영" , groupName: "개발JEANS" },
+        { name: "이수진" , groupName: "덕성전자" },
+        { name: "이유진" , groupName: "HOT GIRL" },
         { name: "이희래" , groupName: "SELECT" },
-        { name: "조승아" , groupName: "" },
-        { name: "최예진" , groupName: "" },
-        { name: "추" , groupName: "" },
-        { name: "한채연" , groupName: "" },
-        { name: "허현우" , groupName: "" }
+        { name: "정현아" , groupName: "HOT GIRL" },
+        { name: "조승아" , groupName: "HOT GIRL" },
+        { name: "최예진" , groupName: "빵야" },
+        { name: "추수현" , groupName: "SELECT" },
+        { name: "한채연" , groupName: "FULL HOUSE" },
+        { name: "허현우" , groupName: "빵야" }
     ];
 
     return (
@@ -52,7 +52,7 @@ const GraduateList = () => {
                                     <Link to={`/developer/${i}`}>
                                     <div className={'d'+i}>
                                         <div className='img'>
-                                            <img src={img} alt='사진' width='150' height='150' />
+                                            <img src={images[m.name]} alt='사진' width='150' height='150' />
                                         </div>
                                         <div className='name'>{m.name}</div>
                                         <div className='group'>{m.groupName}</div>
